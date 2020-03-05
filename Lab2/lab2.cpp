9d27892d317b5bd4268da03c63c7c6c79d8439eb
Lee Korff-Korn <leekk270@gmail.com>
2020-03-05 02:37:07 -0500

    added Lab2

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <ctype.h>
+#include <fstream>
+
+using namespace std;
+
+std::string processes = ""; 
+int n;
+
+
+int main ( int argc, char *argv[] )
+{
+    int count = 0;
+    if ( argc != 2 ) {
+         cout<<"usage: "<< argv[0] <<" <filename>\n";
+    }
+    else {
+        ifstream the_file ( argv[1] );
+        if ( !the_file.is_open() ){
+            cout<<"Could not open file\n";
+        }
+            
+        else {
+            char x;
+            while (the_file.get(x)){
+                if(count == 0){
+                    n = x - '0';
+                }
+                else if(count > 1 && x != ' ' && x != ')' && x != '('){
+                    processes.push_back(x);
+                }
+                count+=1;
+            }      
+        }
+    }
+
+    for(int i = 0; i < processes.size(); i+=4){
+        cout<<i;
+    }
+    //cout<<'\n';
+    cout<< processes;
+    cout<<'\n';
+    cout<<n;
+    cout<<'\n';
+
+  
+}
