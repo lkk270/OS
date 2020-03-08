3491ba6c7b8bafda851ac185eb94fa74fd29f05d
Lee Korff-Korn <leekk270@gmail.com>
2020-03-08 00:18:04 -0500

    data gather from file and sort per row

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -5,9 +5,22 @@
 #include <ctype.h>
 #include <fstream>
 
+#include <iomanip>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <array>
+
 using namespace std;
 
 std::string processes = ""; 
+std::string temp = ""; 
+vector<string> nums;
+// int arr [100][4];
+// array<int, 4> arr[3];
+array<int, 4> arr[100];
+int rowIndex = 0;
+int colIndex = 0;
 int n;
 
 int main ( int argc, char *argv[] )
@@ -27,25 +40,44 @@ int main ( int argc, char *argv[] )
             while (the_file.get(x)){
                 if(count == 0){
                     n = x - '0';
+                    cout<<n;
+                    cout<<'\n';
+                    array<int, 4> arr[n];
                 }
-                else if(count > 1 && x != ' ' && x != ')' && x != '('){
-                    processes.push_back(x);
+                else if(count > 1 && x != '(' && x != ')' && x != ' '){
+                    temp.push_back(x);
+                }
+                else if((x == ' ' || x == ')') && (temp != "" && temp != " ")){
+                    arr[rowIndex][colIndex] = std::stoi(temp);
+                    colIndex++;
+                    temp = "";
+                }
+                else if(x == ')'){
+                    rowIndex++;
+                    colIndex = 0;
+                    temp = "";
                 }
                 count+=1;
             }      
         }
     }
-
-    for(int i = 0; i < processes.size(); i+=4){
-        cout<<i;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 4; j++){
+            cout<<arr[i][j];
+            cout<<' ';
+        }
     }
-    cout<< processes;
-    cout<<'\n';
-    cout<<n;
-    cout<<'\n';
+    cout<<"\n===================================\n";
+    sort( arr, arr + n );
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 4; j++){
+            cout<<arr[i][j];
+            cout<<' ';
+        }
+    }
+
     
-
-
-
+    
   
 }
